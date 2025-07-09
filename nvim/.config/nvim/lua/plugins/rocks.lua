02a88dbbf80cd724fbd60fc5b6aa06631615b47d
abdellah-darni <abdellah.darni.2018@gmail.com>
2025-07-09 16:58:56 +0100

    fixe some bugs

---
+++ nvim/.config/nvim/lua/plugins/rocks.lua
@@ -0,0 +1,13 @@
+-- ~/.config/nvim/lua/plugins/rocks.lua
+return {
+  "nvim-neorocks/rocks.nvim",
+  --opts = {
+  enabled = false,
+   -- hererocks = true,
+  --  rocks = {
+  --    "jsregexp",
+ --     "toml-edit",
+ --   },
+  --},
+--  priority = 1000,
+}
