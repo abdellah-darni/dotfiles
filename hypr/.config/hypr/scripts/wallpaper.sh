ab47692ca2259d4b1edcce2974c1906450e86a1d
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-05-30 20:00:47 +0100

    fixing broken stuff cuz of updates

--- hypr/.config/hypr/scripts/wallpaper.sh
+++ hypr/.config/hypr/scripts/wallpaper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Kill any existing instances to prevent conflicts
-killall swww-daemon
+killall awww-daemon
 
 # Start the daemon in the background
-swww-daemon &
+awww-daemon &
 
 # Wait for the daemon to fully initialize (CRITICAL STEP)
 sleep 1
 
 # Set the wallpaper   thy just move from swww awww .... so this might need updating i m wiating for it to break 
-swww img /home/abde/.config/backgrounds/shaded.png
+awww img /home/abde/.config/backgrounds/shaded.png
